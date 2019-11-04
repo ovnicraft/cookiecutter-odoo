0da6381663933c4b1626125a845d9853d7867e8d
Cristian Salamea <cs@prisehub.com>
2019-11-03 23:01:47 -0500

    [FIX] manifest

--- {{cookiecutter.module_name}}/__manifest__.py
+++ {{cookiecutter.module_name}}/__manifest__.py
@@ -15,17 +15,17 @@
     "license": "{{ cookiecutter.open_source_license }}",
     "application": {%- if cookiecutter.is_application == 'yes' -%} True {% else %} False {% endif %},
     "installable": True,
-    # "pre_init_hook": "pre_init_hook",
-    # "post_init_hook": "post_init_hook",
-    # "post_load": "post_load",
-    # "uninstall_hook": "uninstall_hook",
-    "depends": ["{{ cookiecutter.deps.split(',')|default('base')}}"],
+    {%- if cookiecutter.include_hooks == 'yes'-%}
+     "pre_init_hook": "pre_init_hook",
+    "post_init_hook": "post_init_hook",
+    "post_load": "post_load",
+    "uninstall_hook": "uninstall_hook",
+    {% endif %}
+    "depends": {{ cookiecutter.deps.split(',')|default('base')}},
     "data": [
         "security/security_{{ cookiecutter.main_module }}.xml",
         "security/ir.model.access.csv",
-        "templates/assets.xml",
         "views/view_report.xml",
         "views/view_{{ cookiecutter.main_module }}.xml",
-        "wizards/view_wizard.xml",
     ],
 }
